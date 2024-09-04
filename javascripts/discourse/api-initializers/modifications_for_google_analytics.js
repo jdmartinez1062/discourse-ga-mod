3655c096e6f00b6917821358433a63bca2cdd2d9
Juan David Martinez <juan@discourse.org>
2024-09-04 16:11:48 -0500

    FIX: GetOwner Deprecation warning

--- javascripts/discourse/api-initializers/modifications_for_google_analytics.js
+++ javascripts/discourse/api-initializers/modifications_for_google_analytics.js
@@ -1,8 +1,8 @@
 import { apiInitializer } from "discourse/lib/api";
-import { getOwner } from "discourse-common/lib/get-owner";
 
 export default apiInitializer("0.11.1", api => {
-  const currentUser = getOwner(this).lookup("service:current-user");
+  const router = api.container.lookup("service:router");
+  const currentUser = router.currentUser;
   const userIdObject = window.dataLayer?.find(item => item.userId);
   if (userIdObject) {
     userIdObject["userId"] = currentUser?.external_id;
